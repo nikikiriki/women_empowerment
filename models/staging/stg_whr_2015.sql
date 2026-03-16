c690df7f4a05341decca7743bf898d3ac9bfd016
Nikica Radakovic <kiletina90@gmail.com>
2026-03-16 12:40:19 +0000

    add staging folder, sources.yml and stg_whr_2015

---
+++ models/staging/stg_whr_2015.sql
@@ -0,0 +1,23 @@
+with source as (
+    select * from {{ source('source', 'whr_2015') }}
+),
+
+renamed as (
+    select
+        Country                          as country_name,
+        Region                           as region,
+        `Happiness Rank`                 as happiness_rank,
+        `Happiness Score`                as happiness_score,
+        `Standard Error`                 as standard_error,
+        `Economy _GDP per Capita_`       as gdp_per_capita,
+        Family                           as family_score,
+        `Health _Life Expectancy_`       as health_life_expectancy,
+        Freedom                          as freedom_score,
+        `Trust _Government Corruption_`  as trust_govt_corruption,
+        Generosity                       as generosity_score,
+        `Dystopia Residual`              as dystopia_residual,
+        2015                             as year
+    from source
+)
+
+select * from renamed
