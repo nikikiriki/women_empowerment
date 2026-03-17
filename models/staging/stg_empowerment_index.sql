227106e4929ec909b6ca38ade29acc783681740b
gianni di lonardo <dilonardo.gianni@gmail.com>
2026-03-17 16:19:57 +0000

    fixed to be able to see the data

--- models/staging/stg_empowerment_index.sql
+++ models/staging/stg_empowerment_index.sql
@@ -3,22 +3,16 @@ with source as (
 ),
 
 renamed as (
-    select
-        Country as country,
-        
-        "Women's Empowerment Index (WEI) - 2022" as womens_empowerment_index,
-        
-        "Women's Empowerment Group - 2022" as womens_empowerment_group,
-        
-        "Global Gender Parity Index (GGPI) - 2022" as gender_parity_index,
-        
-        "Gender Parity Group - 2022" as gender_parity_group,
-        
-        "Human Development Group - 2021" as human_development_group,
-        
-        "Sustainable Development Goal regions" as sdg_regions
-
-    from source
+  select
+    Country as country,
+    -- Usa il nome esatto suggerito dall'errore, senza le parentesi tonde se non ci sono
+    `Women's Empowerment Index _WEI_ - 2022` as womens_empowerment_index,
+    `Women's Empowerment Group - 2022` as womens_empowerment_group,
+    `Global Gender Parity Index _GGPI_ - 2022` as gender_parity_index,
+    `Gender Parity Group - 2022` as gender_parity_group,
+    `Human Development Group - 2021` as human_development_group,
+    `Sustainable Development Goal regions` as sdg_regions
+  from source
 )
 
 select * from renamed
